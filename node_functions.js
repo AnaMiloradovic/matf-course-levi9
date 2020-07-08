3263e07ec7c396f8c77e9679db9f2fffaae73eb6
AnaMiloradovic <ana.miloradovic7@gmail.com>
2020-07-08 22:51:58 +0200

    adding node part

---
+++ node_functions.js
@@ -0,0 +1,41 @@
+function loadUsername() {
+    let person = prompt("Please enter your name", "Nevena Soldat");
+    let username = "";
+
+    if (person == null || person == "") {
+        username = "Unknown username";
+    } else {
+        username = person;
+    }
+    
+    return username;
+};
+
+
+const sendResult = async (username, result) => {
+    try { 
+        const URL = 'http://localhost:3002/';
+        const response = await fetch(URL, {
+            method : 'POST',
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            mode : 'cors',
+            body : JSON.stringify({
+                name : username,
+                score : result
+            })
+        });
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function showScore() {
+    gameContext.font = "16px Arial";
+    gameContext.fillStyle = "#585858";
+    gameContext.fillText("Score: " + score, 980, 20);
+}
