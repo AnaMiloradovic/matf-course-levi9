c4a8f399049adcf575cf0eac8fe2461e01461630
AnaMiloradovic <ana.miloradovic7@gmail.com>
2020-07-09 19:01:02 +0200

    final changes

--- snake.js
+++ snake.js
@@ -12,13 +12,12 @@ const box = 32;
 // load images
 
 const ground = new Image();
-ground.src = "img/sand.jpeg";
+ground.src = "img/sand.png";
 
 const foodImg = new Image();
-foodImg.src = "img/food1.png";
+foodImg.src = "img/food.png";
 
 // load audio files
-
 let dead = new Audio();
 let eat = new Audio();
 let up = new Audio();
@@ -29,13 +28,6 @@ let down = new Audio();
 
 ucitajIme();
 
-
-// dead.src = "audio/dead.mp3";
-// eat.src = "audio/eat.mp3";
-// up.src = "audio/up.mp3";
-// right.src = "audio/right.mp3";
-// left.src = "audio/left.mp3";
-// down.src = "audio/down.mp3";
 
 // create the snake
 
@@ -158,6 +150,4 @@ function draw(){
 // call draw function every 100 ms
 
 let game = setInterval(draw,100);
-
-
 
